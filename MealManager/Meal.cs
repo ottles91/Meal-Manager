13173049181a122a2af9792ebadcbd30a8e6218c
Cameron Ottley <cameron.ottley@gmail.com>
2023-05-16 17:30:03 -0600

    Created initial Meal object and driver class

---
+++ MealManager/Meal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MealManager
+{
+	public class Meal
+	{
+		private string Name { get; }
+		//TODO: Add a list of ingredients
+
+		public Meal()
+		{
+		}
+
+		public Meal(string name)
+		{
+			Name = name;
+		}
+	}
+}
+

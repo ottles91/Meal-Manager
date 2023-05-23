51a30588bd63e27a872c420ba7732e96854ceb03
Cameron Ottley <cameron.ottley@gmail.com>
2023-05-23 14:38:39 -0600

    Created initial UI and added support for creating a Meal object using a string

---
+++ MealManager/MealBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace MealManager
+{
+	public class MealBook
+	{
+		public List<Meal> meals { get; }
+
+		//Takes a meal object and adds it to the master list
+		public bool addMeal(Meal meal)
+		{
+			if(meal == null)
+			{
+				return false;
+			}
+			else
+			{
+				meals.Add(meal);
+				return true; 
+			}
+		}
+
+		//Takes a string, creates a meal object and adds it to the master list
+		public bool addMeal(string meal)
+		{
+			if(meal == null)
+			{
+				return false;
+			}
+			else
+			{
+				Meal meal1 = new Meal(meal);
+				meals.Add(meal1);
+				return true; 
+			}
+		}
+
+		public MealBook()
+		{
+			//Initiate the mast list
+			meals = new List<Meal>();
+		}
+	}
+}
+
--- MealManager/Program.cs
+++ MealManager/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MealManager;
 
+bool terminateProgram = false;
+
+//Create the MealBook object to handle the meals
+MealBook book = new MealBook();
+
+void DisplayMenu()
+{
+    while (!terminateProgram)
+    {
+        Console.WriteLine("Welcome! Enter a meal name to add it to your Meal Book, or type quit to exit");
+        string input = Console.ReadLine();
+
+        //Quit the program if the user entered "quit"
+        if (input.ToLower() == "quit")
+        {
+            Console.WriteLine("Goodbye!");
+            terminateProgram = true;
+            //Console.ReadKey();
+        }
+        else
+        {
+            bool sucessfullyAdded = ProcessInput(input);
+
+            //If the meal was sucessfully added then inform the user and go back to the top
+            if(sucessfullyAdded)
+            {
+                Console.WriteLine("Successfully added!");
+                continue;
+            }
+        }
+
+    }
+}
+
+bool ProcessInput(string input)
+{
+    if (input == null)
+    {
+        return false;
+    }
+    else
+    {
+        //Create a new Meal object with the name inputted by the user
+        bool successfullyAdded = book.addMeal(input);
+        return successfullyAdded;
+    }
+}
+
+DisplayMenu();
